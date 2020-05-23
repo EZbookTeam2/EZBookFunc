794848abcb96e36d40c94a823aaf592f985dc326
VuiTung <TP050801@mail.apu.edu.my>
2020-05-23 14:41:39 +0800

    added cancellationListcontroller, updated cancellationmodel change 0

---
+++ Testing9/Controllers/CancellationListController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Testing9.Models;
+
+namespace Testing9.Controllers
+{
+    public class Data
+    {
+        public string CancelId { get; set; }
+        public string BookingId { get; set; }
+        public string RoomName { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Username { get; set; }
+        public string Status { get; set; }
+        public List<string> CancellationList { get; set; }
+    }
+    [RoutePrefix("api/CancellationList")]
+    public class CancellationListController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            
+            using (EZbookContext db = new EZbookContext())
+            {
+                List<Data> CancellationList = (from c in db.Cancellation
+                                                 join d in db.Booking
+                                                 on c.BookingId equals d.BookingId
+                                                 join e in db.Users on d.UsersId equals e.UsersId
+                                                 select new Data
+                                                 {   Username = e.Names,
+                                                     CancelId = c.CancellationId,
+                                                     BookingId = d.BookingId,
+                                                     RoomName = d.Name,
+                                                     Title = d.Title,
+                                                     Location = d.Location,
+                                                     Time = d.Time,
+                                                     Date = d.Date,
+                                                 }).ToList();
+                
+
+
+                return Ok(CancellationList);
+            }
+        }
+    }
+}
