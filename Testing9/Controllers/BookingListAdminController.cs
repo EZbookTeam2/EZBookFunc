8a0ef79a226f1e12175be498e8b18869b867aab4
VuiTung <TP050801@mail.apu.edu.my>
2020-05-29 23:47:39 +0800

    bookinglsit admin fixed and cancelcheck added

--- Testing9/Controllers/BookingListAdminController.cs
+++ Testing9/Controllers/BookingListAdminController.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Web.Http;
 using Testing9.Models;
 using Testing9.Utils;
 
 namespace Testing9.Controllers
 {
+    public class cancelbooking
+    {
+        public string BookingId { get; set; }
+        public string UsersId { get; set; }
+        public string RoomId { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Status { get; set; }
+        public string Username { get; set; }
+
+        public List<string> BookingList { get; set; }
+    }
     [RoutePrefix("api/BookingListAdmin")]
     public class BookingListAdminController : ApiController
     {
@@ -19,9 +36,27 @@ namespace Testing9.Controllers
             List<Booking> bookingList = new List<Booking>();
             using (ezbookdatabaseContext db = new ezbookdatabaseContext())
             {
-                bookingList = db.Booking.Where(a => a.Status == "Approved").OrderBy(a => a.BookingId).ToList();
+                List<cancelbooking> BookingList = (from b in db.Users
+                                                   join c in db.Booking
+                                                   on b.UsersId equals c.UsersId
+                                                   where (c.Status == "Approved")
+                                                   orderby (c.BookingId) descending
+                                                   select new cancelbooking
+                                                   {
+                                                   
+                                                       BookingId = c.BookingId,
+                                                       UsersId = b.UsersId,
+                                                       RoomId = c.RoomId,
+                                                       Name = c.Name,
+                                                       Title = c.Title,
+                                                       Location = c.Location,
+                                                       Date = c.Date,
+                                                       Time = c.Time,
+                                                       Status = c.Status,
+                                                       Username = b.Names
+                                                   }).ToList();
 
-                return Ok(bookingList);
+                return Ok(BookingList);
             }
         }
     }
---
+++ Testing9/Controllers/cancelcheckController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Testing9.Models;
+using Testing9.Utils;
+namespace Testing9.Controllers
+{
+    [RoutePrefix("api/cancelCheck")]
+    public class cancelcheckController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            List<Cancellation> cancelList = new List<Cancellation>();
+            using (ezbookdatabaseContext db = new ezbookdatabaseContext())
+            {
+                cancelList = db.Cancellation.Where(a => a.Status == "New").OrderBy(a => a.CancellationId).ToList();
+
+                return Ok(cancelList);
+            }
+        }
+    }
+}
