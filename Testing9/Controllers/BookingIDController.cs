1a869ab3681acef0b1ba3495c465e3bc95c8eaa4
kopidadoudou <kelvintee1999@gmail.com>
2020-06-01 04:50:58 +0800

    add post function in bookinglistcancel

---
+++ Testing9/Controllers/BookingIDController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Testing9.Models;
+
+namespace Testing9.Controllers
+{
+    [RoutePrefix("api/BookingID")]
+
+    public class BookingIDController : ApiController
+    {
+        ezbookdatabaseContext dbContext = new ezbookdatabaseContext();
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            List<Booking> bookingList = new List<Booking>();
+            using (ezbookdatabaseContext db = new ezbookdatabaseContext())
+            {
+                bookingList = db.Booking.ToList();
+
+                return Ok(bookingList);
+            }
+        }
+    }
+}
