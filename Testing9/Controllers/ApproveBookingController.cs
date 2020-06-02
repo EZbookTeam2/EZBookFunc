afc3f34f7e9ea4b3148ac62bf1042fd1cf6baf4d
VuiTung <TP050801@mail.apu.edu.my>
2020-06-02 21:09:15 +0800

    approvebooking and approvecancel

---
+++ Testing9/Controllers/ApproveBookingController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Testing9.Models;
+
+namespace Testing9.Controllers
+{
+    [RoutePrefix("api/ApproveBooking")]
+    public class ApproveBookingController : ApiController
+    {
+        ezbookdatabaseContext dbContext = new ezbookdatabaseContext();
+        [HttpPut]
+        public IHttpActionResult Put(Booking value)
+        {
+            try
+            {
+                var data = from b in dbContext.Booking
+                           where b.BookingId == value.BookingId
+                           select b;
+                Booking old = data.SingleOrDefault();
+                old.Status = value.Status;
+                dbContext.SaveChanges();
+                return Ok("hello");
+            }
+            catch (Exception ex)
+            {
+                string message = "fail " + ex.Message;
+                return Ok(message);
+
+            }
+        }
+    }
+}
