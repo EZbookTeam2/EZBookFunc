7c8b252f0749dbdbb34016731fe7d88c11175a83
VuiTung <TP050801@mail.apu.edu.my>
2020-06-03 19:58:06 +0800

    cancelBookingcontroller

---
+++ Testing9/Controllers/CancelBookingController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Testing9.Models;
+
+namespace Testing9.Controllers
+{
+    [RoutePrefix("api/CancelBooking")]
+    public class CancelBookingController : ApiController
+    {
+        ezbookdatabaseContext dbContext = new ezbookdatabaseContext();
+        [HttpPut]
+        public IHttpActionResult Put(Booking value)
+        {
+            try
+            {
+                var data = from b in dbContext.Booking
+                           where b.BookingId == value.BookingId
+                           select b;
+                Booking old = data.SingleOrDefault();
+                old.Status = "Cancelled";
+                dbContext.SaveChanges();
+                return Ok("Successful");
+            }
+            catch (Exception ex)
+            {
+                string message = "fail " + ex.Message;
+                return Ok(message);
+
+            }
+        }
+    }
+}
