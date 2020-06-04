f447d62bf5a83f1b4c1c6cab397d3d6dc6a11dde
VuiTung <TP050801@mail.apu.edu.my>
2020-06-04 21:27:16 +0800

    delete user working change 1

---
+++ Testing9/Controllers/DeleteUserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Windows.Forms;
+using Testing9.Models;
+
+namespace Testing9.Controllers
+{
+    [RoutePrefix("api/DeleteUser")]
+    public class DeleteUserController : ApiController
+    {
+        ezbookdatabaseContext dbContext = new ezbookdatabaseContext();
+
+        [HttpDelete]
+        public IHttpActionResult DeleteByName([FromUri]string id = null)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return BadRequest("empty");
+            }
+            try
+            {
+                var data = from user in dbContext.Users
+                           where user.UsersId == id
+                           select user;
+                Users obj = data.SingleOrDefault();
+                dbContext.Users.Remove(obj);
+                dbContext.SaveChanges();
+                return Ok("Delete successfully");
+            }
+            catch (Exception Ex)
+            {
+                return Ok(Ex);
+            }
+        }
+            
+        
+        }
+}
